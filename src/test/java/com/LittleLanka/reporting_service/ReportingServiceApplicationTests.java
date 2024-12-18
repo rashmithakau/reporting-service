e5805a7619f7d06b916946a4a270314ae3942a52
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2024-12-18 09:07:37 +0530

    Initialized the project

---
+++ src/test/java/com/LittleLanka/reporting_service/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LittleLanka.reporting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
